c4fbceda707a4f9fb8c71b35c82fbddcf62c2e28
Misael Sabán <misaelsaban@gmail.com>
2024-06-12 17:22:22 -0300

    se actualiza repo

--- wp-content/plugins/mage-eventpress/templates/single/details.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo apply_filters('mep_event_details_content', $content, get_the_id());
--- wp-content/plugins/seatreg/js/utils.js
+++
@@ -1,6 +0,0 @@
-function setCalendarDateUrlParam(calendarDate) {
-	var queryParams = new URLSearchParams(window.location.search);
-
-	queryParams.set('calendar-date', calendarDate);
-	window.history.replaceState(null, null, '?' + queryParams.toString());
-}
